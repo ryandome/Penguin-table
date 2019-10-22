fdc634266ecd25e2b126192f37c20f4e9b104c22
Ryan Dome <ryan.dome@centre.edu>
2019-10-22 10:58:22 -0400

    update

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
+var penguinPromise = d3.json("classData.json");
 
+penguinPromise.then(
+function(classData)
+    {
+        var penguin = classData[0]
+        console.log(penguin.quizes);
+        console.log(penguin.quizes.map(grade));
+        //getGrade(classData);
+        console.log("classroom",classData)
+    },
+function(err)
+    {
+        console.log("broken",err)
+    }
+)
+
+
+//var getGrade = function(quiz)
+//{
+    //return quiz.grade
+//}
+//getGrade(classroom[0],quizes[0])
